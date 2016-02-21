b1a3e5dfce34f6af7ceb3e6952d7fa9f1d58b015
Jeremy Sik <jeremy.sik@me.com>
2016-02-21 17:27:08 +1100

    Updating AppUser model, changing project name and making User model private.

--- common/models/user/app-user.ts
+++ common/models/user/app-user.ts
@@ -1,20 +1,20 @@
-function appUser(AppUser:any) {
+export module common.models.user {
+    function appUser(AppUser:any) {
 
-    AppUser.greet = function (msg:string, cb:Function) {
+        AppUser.greet = function (msg:string, cb:Function) {
 
-        console.log(AppUser);
+            console.log(AppUser);
 
-        cb(null, 'Greetings... ' + msg);
-    };
+            cb(null, 'Greetings... ' + msg);
+        };
 
-    AppUser.remoteMethod(
-        'greet',
-        {
-            accepts: {arg: 'msg', type: 'string'},
-            returns: {arg: 'greeting', type: 'string'}
-        }
-    );
+        AppUser.remoteMethod(
+            'greet',
+            {
+                accepts: {arg: 'msg', type: 'string'},
+                returns: {arg: 'greeting', type: 'string'}
+            }
+        );
 
+    }
 }
-
-export = appUser;
